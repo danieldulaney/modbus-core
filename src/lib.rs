af40e102958f88785ac8f1865aa13685807466ea
Daniel Dulaney <dan@dulaney.xyz>
2020-03-15 00:39:16 -0400

    feat: add protocols module
    Protocols are zero-sized types that implement the ModbusProtocol trait. This
    trait provides several functions relevant to chopping up a MODBUS ADU into its
    constituent parts. At the moment TCP MODBUS is fully supported, MODBUS RTU's
    skeleton is laid out, and MODBUS ASCII is not at all supported.
    
    No tests are included at this time.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 #![no_std]
 
 pub mod bit_pack;
+pub mod protocols;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Coil {
     On,
     Off,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ModbusError {
+    BadFuncCode,
+    BadErrorCheck,
+    NotEnoughData,
 }
