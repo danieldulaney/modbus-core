fddd63432690b6ed76bab074b2659d9a13b0a399
Daniel Dulaney <dan@dulaney.xyz>
2020-03-16 12:32:23 -0400

    docs: update ModbusProtocol docs
    Update the ModbusProtocol docs to match API changes.

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -1,4 +1,4 @@
-//! Tools for implementing MODBUS over various transport layers
+//! Tools for implementing MODBUS over various transport layers.
 //!
 //! The MODBUS protocol can be transported over different media, each of which wraps the MODBUS
 //! message slightly differently. The inner MODBUS message is known as the protocol data unit (PDU),
@@ -16,41 +16,39 @@ use crate::ModbusError;
 mod modbus_rtu;
 mod tcp_modbus;
 
-/// A MODBUS transport protocol
+/// A MODBUS transport protocol.
 ///
 /// Generally, this is implemented on a zero-sized type.
 pub trait ModbusProtocol {
     /// The maximum allowable length of an Application Data Unit in this protocol
     const ADU_MAX_LENGTH: usize;
 
-    /// A type representing the header for this particular packet
+    /// A type representing the header for this particular packet.
     ///
     /// It is not necessarily bit-compatible with the underlying representation.
     type Header: core::fmt::Debug + Clone;
 
-    /// Extract the length of the given ADU
-    ///
-    /// If there is not enough data to extract the length, return None.
+    /// Extracts the length of the given ADU.
     ///
     /// If determining the length information requires examining the function code, an unrecognized
-    /// function code is represented by `Some(Err(BadFuncCode))`.
+    /// function code is represented by `Err(BadFuncCode)`.
+    ///
+    /// If the ADU length is out of bounds (either too big or too small), returns `Err(BadLength)`.
     fn adu_length(data: &[u8]) -> Result<usize, ModbusError>;
 
-    /// Extract the header data associated with the given ADU
-    ///
-    /// If there is not enough data to extract a complete header, return None.
+    /// Extracts the header data associated with the given ADU.
     ///
     /// If determining the header information requires examining the function code, an unrecognized
-    /// function code is represented by `Some(Err(BadFuncCode))`.
+    /// function code is represented by `Err(BadFuncCode)`.
     fn adu_header(data: &[u8]) -> Result<Self::Header, ModbusError>;
 
-    /// Determine if the ADU matches the checksum
+    /// Determines if the ADU checksum is valid.
     ///
     /// If determining the checksum status requires examining the function code, an unrecognized
-    /// function code is represented by `Some(Err(BadFuncCode))`.
+    /// function code is represented by `Err(BadFuncCode)`.
     fn adu_check(data: &[u8]) -> Result<(), ModbusError>;
 
-    /// Get the header information the inner PDU data, checking the checksum first
+    /// Get the header information the inner PDU data, checking the checksum first.
     fn pdu_body(data: &[u8]) -> Result<&[u8], ModbusError>;
 }
 
